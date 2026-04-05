0c584b62da6797ab8338c83e38202993c6bf2e90
aznoh <glentik23@gmail.com>
2026-04-05 16:28:50 +0200

    user create and get

--- src/controllers/userController.mjs
+++ src/controllers/userController.mjs
@@ -1,13 +1,62 @@
-export const createUser = (req, res) => {
-    res.status(501).send()
+import { randomUUID } from 'crypto';
+import User from '../models/User.mjs';
+
+const USER_PUBLIC_FIELDS = '-_id -loginPassword'
+
+export const createUser = async (req, res) => {
+    const { firstName, lastName, loginName, loginPassword } = req.body
+
+    if (!firstName || !lastName || !loginName || !loginPassword) {
+        return res.status(400).json({
+            message: 'firstName, lastName, loginName, and loginPassword are required',
+        })
+    }
+
+    try {
+        const user = await User.create({
+            id: randomUUID(),
+            firstName,
+            lastName,
+            loginName,
+            loginPassword,
+        })
+
+        return res.status(201).json(user)
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Failed to create user',
+        })
+    }
 }
 
-export const getUsers = (req, res) => {
-    res.status(501).send()
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({}, USER_PUBLIC_FIELDS)
+
+        return res.status(200).json(users)
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Failed to get users',
+        })
+    }
 }
 
-export const getUserById = (req, res) => {
-    res.status(501).send()
+export const getUserById = async (req, res) => {
+    try {
+        const user = await User.findOne({ id: req.params.id }, USER_PUBLIC_FIELDS)
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'User not found',
+            })
+        }
+
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Failed to get user',
+        })
+    }
 }
 
 export const updateUser = (req, res) => {
