ad252c486d7a8f1ff06288f4d243bf2a1e92f2fb
aznoh <glentik23@gmail.com>
2026-05-07 10:21:19 +0200

    added audio prerecord

--- gateway/src/index.mjs
+++ gateway/src/index.mjs
@@ -1,10 +1,21 @@
 import mic from "mic";
 import 'dotenv/config'
-
+import wav from "wav"
+import fs from "node:fs";
 
 const SAMPLE_RATE = 16000;
 const CHANNELS = 1;
 const BIT_DEPTH = 16;
+const BYTES_PER_SAMPLE = BIT_DEPTH / 8;
+const PRE_RECORD_SECONDS = 1;
+
+
+
+// rolling buffer
+const preRecordChunks = [];
+let preRecordBytes = 0;
+const MAX_PRE_RECORD_BYTES = SAMPLE_RATE * CHANNELS * BYTES_PER_SAMPLE * PRE_RECORD_SECONDS;
+
 
 const STATES = {
     IDLE: "idle",
@@ -13,11 +24,17 @@ const STATES = {
     TALKING: "talking"
 }
 
+const NOISE_LEVEL = {
+    LOW: "low",
+    MEDIUM: "medium",
+    HIGH: "high"
+}
+
 
 const globalState = {
     currentState: STATES.IDLE,
-    noiseLevelAccumulator: [],
-    averageNoiseLevel: 0,
+    currentNoiseLevel: NOISE_LEVEL.LOW,
+    noiseAccumulator: 0
 }
 
 
@@ -45,12 +62,12 @@ function main(){
         const rms = calculateRms(data);
         const db = rmsToDb(rms);
         const level = classifyNoise(db);
+        globalState.currentNoiseLevel = level;
 
-        console.log({
-            rms: rms.toFixed(4),
-            db: db.toFixed(1),
-            level,
-        });
+        if (globalState.isRecording) {
+            globalState.currentFileWriter.write(data);
+        }
+		addToPreRecordBuffer(data);
     });
 
     globalState.audioInputStream.on("error", (err) => {
@@ -60,28 +77,65 @@ function main(){
 
     console.log("starting primary loop");
     // launch primare loop
-    setInterval(primaryLoop, 300);
+    setInterval(primaryLoop, 50);
 }
 
 
 
 
 function primaryLoop() {
+
+    //console.log("current noise level :", globalState.currentNoiseLevel, "accumulator:", globalState.noiseAccumulator);
     // handle current state
     switch (globalState.currentState) {
         case STATES.IDLE:
             handleIdleState();
             break;
+        case STATES.LISTENING:
+            handleListeningState();
+            break;
     }
+
 }
 
 
 function handleIdleState() {
+    if (globalState.currentNoiseLevel === NOISE_LEVEL.MEDIUM) {
+        globalState.noiseAccumulator += 1;
+    }
+    else if (globalState.currentNoiseLevel === NOISE_LEVEL.HIGH) {
+        globalState.noiseAccumulator += 3;
+    }
+    else if (globalState.currentNoiseLevel === NOISE_LEVEL.LOW && globalState.noiseAccumulator > 0) {
+        globalState.noiseAccumulator -= 1;
+    }
 
+    if (globalState.noiseAccumulator > 5) {
+        // start listening
+        startRecording();
+        globalState.noiseAccumulator = 0;
+        globalState.currentState = STATES.LISTENING;
+    }
 }
 
 
-// region noise functions
+function handleListeningState() {
+    if (globalState.currentNoiseLevel === NOISE_LEVEL.LOW) {
+        globalState.noiseAccumulator += 1;
+    }else {
+        globalState.noiseAccumulator = 0;
+    }
+
+
+    if (globalState.noiseAccumulator > 10) {
+        globalState.currentState = STATES.IDLE;
+        globalState.noiseAccumulator = 0;
+        stopRecording();
+    }
+}
+
+
+// region noise calculation functions
 const QUIET_THRESHOLD = process.env.QUIET_THRESHOLD;
 const LOUD_THRESHOLD = process.env.LOUDNESS_THRESHOLD;
 
@@ -108,12 +162,61 @@ function rmsToDb(rms) {
 }
 
 function classifyNoise(db) {
-    if (db < QUIET_THRESHOLD) return "quiet";
-    if (db < LOUD_THRESHOLD) return "normal";
-    return "loud";
+    if (db < QUIET_THRESHOLD) return NOISE_LEVEL.LOW;
+    if (db < LOUD_THRESHOLD) return NOISE_LEVEL.MEDIUM;
+    return NOISE_LEVEL.HIGH;
 }
+
+function addToPreRecordBuffer(chunk) {
+ 	preRecordChunks.push(chunk);
+  	preRecordBytes += chunk.length;
+
+  	while (preRecordBytes > MAX_PRE_RECORD_BYTES) {
+    	const removedChunk = preRecordChunks.shift();
+    	preRecordBytes -= removedChunk.length;
+  	}
+}
+
+
 // endregion
 
+// region recording functions
+let recordingCounter = 0;
 
+function startRecording() {
+    const filename = `recording-${recordingCounter}.wav`;
+    recordingCounter++;
+
+	
+    globalState.currentFileWriter = new wav.FileWriter(filename, {
+        sampleRate: SAMPLE_RATE,
+        channels: CHANNELS,
+        bitDepth: BIT_DEPTH,
+    });
+
+	// push prerecord buffer to current recording
+	for (const bufferedChunk of preRecordChunks) {
+	    globalState.currentFileWriter.write(bufferedChunk);
+  	}
+
+
+
+	// start recording
+    globalState.isRecording = true;
+
+    console.log(`Started recording: ${filename}`);
+}
+
+function stopRecording() {
+    if (!globalState.currentFileWriter) return;
+
+    globalState.currentFileWriter.end();
+
+    globalState.currentFileWriter = null;
+    globalState.isRecording = false;
+
+    console.log("Stopped recording");
+}
+// endregion
 
 main();
